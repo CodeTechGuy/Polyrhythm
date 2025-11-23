deab9191e424ec9625a695825070cb3cddeb8c5a
CodeTechGuy <vishalprajwal.s@gmail.com>
2025-11-23 20:46:31 +0530

    sound and ball-arrangement sorted

---
+++ ball.js
@@ -0,0 +1,28 @@
+class Ball {
+    constructor(track, radius , speed , soundFrequency){
+        this.track = track;
+        this.radius = radius;
+        this.speed = speed
+        this.soundFrequency = soundFrequency
+        this.offset = 0
+        this.direction = 1
+        this.center = this.track.getPosition(this.offset)
+    }
+
+    move() {
+        this.offset += this.speed * this.direction;
+        this.center = this.track.getPosition(this.offset)
+        if (this.center.y > this.track.center.y) {
+            this.direction *= -1
+            playSound(this.soundFrequency)
+
+        }
+    }
+
+    draw(ctx){
+        ctx.beginPath()
+        ctx.arc(this.center.x , this.center.y , this.radius , 0 , Math.PI * 2)
+        ctx.strokeStyle = 'white'
+        ctx.stroke()
+    }
+}
