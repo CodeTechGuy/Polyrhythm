2d740d4e8a5d708f3cf820d03f2f3b0aaa0e7481
CodeTechGuy <vishalprajwal.s@gmail.com>
2025-11-23 20:46:58 +0530

    sound and balls-arrangement sorted

--- track.js
+++ track.js
@@ -4,9 +4,21 @@ class Track {
         this.radius = radius;
     }
 
+    getPosition(offset) {
+        return {
+            x: this.center.x + Math.cos(offset) * this.radius,
+            y: this.center.y - Math.sin(offset) * this.radius
+        }
+    }
+
     draw(ctx){
         ctx.beginPath();
-        ctx.arc(this.center.x , this.center.y , this.radius , 0 , Math.PI * 2);
+        // ctx.arc(this.center.x , this.center.y , this.radius , 0 , Math.PI * 2);
+        for (let a = 0; a < Math.PI * 2; a += 0.1){
+            const pos = this.getPosition(a)
+            ctx.lineTo(pos.x , pos.y)
+        }
+        ctx.closePath()
         ctx.strokeStyle = 'white';
         ctx.stroke();
     }
